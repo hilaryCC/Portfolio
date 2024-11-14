89c629fceadb8fc5e4284c50853adb8416e42875
Hilary <hilaryc.cr@gmail.com>
2024-11-13 23:26:24 -0600

    about update

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className='hero container'>
         <div className="hero-text">
             <h2>Hi, I'm Hilary Castro. </h2>
-            <h1>Welcome to my website portfolio</h1>
+            <h1>Welcome to my website</h1>
             <p>
                 I'm a Computer Engineering graduate with college experience in game, web, console and desktop app 
                 development and static/dinamic testing; profesional experience as a Cloud Engineer working with 
--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -5,14 +5,37 @@ import image1 from '../../assets/blacklogo.png'
 const About = () => {
   return (
     <div className='about'>
-        <div className="section">
-          <h3>ABOUT</h3>
+        <div className="left">
+          <div className="section">
+            <h3>ABOUT</h3>
+            <p>
+              I'm a 23 year old Computer Engineer woman with passion
+              for learn and create. I enjoy to bring new ideas to life 
+              through teamwork and asertive communication.
+            </p>
+            <p>
+              Most of my projects where build during collegue, find 
+              my re-uploaded repositories here (insert github link)
+            </p>
+            <p>
+              When I'm not coding or learning a technology I like to 
+              spend my time on my hobbies:
+            </p>
+            <ul>
+              <li>Photography</li>
+              <li>Crochet</li>
+              <li>Music</li>
+              <li>Lego building</li>
+            </ul>
+          </div>
         </div>
-        <div className="section">
-          <h3>EDUCATION</h3>
-        </div>
-        <div className="section">
-          <h3>SKILLS</h3>
+        <div className="right">
+          <div className="section top">
+            <h3>EDUCATION</h3>
+          </div>
+          <div className="section bottom">
+            <h3>SKILLS</h3>
+          </div>
         </div>
     </div>
   )
